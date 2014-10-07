bddc1955e7df038ceef457f999f15c0029f5a322
Valentin Rondel <vrondel@insa-rennes.fr>
2014-10-07 08:30:07 +0200

    first commit

---
+++ src/main/java/vrondel/App.java
@@ -0,0 +1,15 @@
+package vrondel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    /** je suis un commentaire */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
